d7e6deb0279e0a65d5f5a7f8ef1c63e03ab93c4f
Igor Souza <igorsouza.dev@gmail.com>
2021-05-27 09:53:25 +0200

    Change main file

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,17 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>Nextjs boilerplate</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="React avancado" />
+    <S.Title>Nextjs boilerplate</S.Title>
+    <S.Description>
+      Typescript, ReactJS, NextJS and Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="developer in front of a computer"
+    />
+  </S.Wrapper>
 )
 
 export default Main
